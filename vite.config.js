fa5c19b3234c00375b4b921238f26149ba85e760
Eli Wilz <35387236+eliaswilz@users.noreply.github.com>
2026-04-17 21:12:22 -0500

    Add multi-page build entries to vite config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,20 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 import MarkdownPlugin from '@goodforyou/vite-plugin-markdown-import'
 
 export default defineConfig({
   server: {
     allowedHosts: true
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        lists: resolve(__dirname, 'lists.html'),
+        essays: resolve(__dirname, 'essays.html'),
+        booking: resolve(__dirname, 'booking.html'),
+      }
+    }
   },
   plugins: [
     MarkdownPlugin()
